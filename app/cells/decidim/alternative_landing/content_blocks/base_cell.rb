a4bb226976e14187fd07ac0ce2da2f5c1a252b15
Vera Rojman <vrojman@protonmail.com>
2021-06-15 11:12:51 +0200

    Add snippets code

--- app/cells/decidim/alternative_landing/content_blocks/base_cell.rb
+++ app/cells/decidim/alternative_landing/content_blocks/base_cell.rb
@@ -6,10 +6,11 @@ module Decidim
       class BaseCell < Decidim::ViewModel
         include Decidim::SanitizeHelper
         include Decidim::TranslationsHelper
-        include Decidim::NeedsSnippets
+
+        delegate :snippets, to: :controller
 
         def show
-          snippets.add(:head, stylesheet_link_tag("decidim/alternative_landing/application"))
+          snippets.add(:alternative_landing, stylesheet_link_tag("decidim/alternative_landing/application"))
           super
         end
       end
