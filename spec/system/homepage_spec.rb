22276589047c264b524da4c036018e3e78cdef38
Vera Rojman <vrojman@protonmail.com>
2021-06-29 13:49:26 +0200

    Add specs for blog posts and meetings blocks

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -20,6 +20,12 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
     let!(:stack_horizontal_block) { create(:stack_horizontal_block, organization: organization) }
     let!(:stack_vertical_block) { create(:stack_vertical_block, organization: organization) }
     let!(:tiles_block) { create(:tiles_block, organization: organization) }
+    let!(:latest_blog_posts_block) { create(:latest_blog_posts_block, organization: organization) }
+    let!(:upcoming_meetings_block) { create(:upcoming_meetings_block, organization: organization) }
+    let!(:blogs_component) { create(:component, manifest_name: "blogs", organization: organization) }
+    let!(:meetings_component) { create(:component, manifest_name: "meetings", organization: organization) }
+    let!(:blog_posts) { create_list(:post, 6, component: blogs_component) }
+    let!(:meetings) { create_list(:meeting, 6, :upcoming, component: meetings_component) }
 
     before do
       visit decidim.root_path
@@ -32,6 +38,8 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
       expect(page).to have_selector(".alternative-landing.stack-horizontal")
       expect(page).to have_selector(".alternative-landing.stack-vertical")
       expect(page).to have_selector(".alternative-landing.tiles-4")
+      expect(page).to have_selector(".alternative-landing.latest-blog-posts")
+      expect(page).to have_selector(".alternative-landing.upcoming-meetings")
     end
 
     describe "cover_full block" do
@@ -147,6 +155,36 @@ describe "Visit the home page", type: :system, perform_enqueued: true do
                 end
               end
             end
+          end
+        end
+      end
+    end
+
+    describe "latest_blog_posts block" do
+      it "renders all elements" do
+        within ".alternative-landing.latest-blog-posts" do
+          expect(page).to have_i18n_content(latest_blog_posts_block.settings.title, upcase: true)
+          expect(page).to have_i18n_content(latest_blog_posts_block.settings.link_text, upcase: true)
+          blog_posts.last(3).each do |blog_post|
+            expect(page).to have_i18n_content(blog_post.title)
+          end
+          blog_posts.first(3).each do |blog_post|
+            expect(page).not_to have_i18n_content(blog_post.title)
+          end
+        end
+      end
+    end
+
+    describe "upcoming_meetings block" do
+      it "renders all elements" do
+        within ".alternative-landing.upcoming-meetings" do
+          expect(page).to have_i18n_content(upcoming_meetings_block.settings.title, upcase: true)
+          expect(page).to have_i18n_content(upcoming_meetings_block.settings.link_text, upcase: true)
+          meetings.last(3).each do |meeting|
+            expect(page).to have_i18n_content(meeting.title)
+          end
+          meetings.first(3).each do |meeting|
+            expect(page).not_to have_i18n_content(meeting.title)
           end
         end
       end
