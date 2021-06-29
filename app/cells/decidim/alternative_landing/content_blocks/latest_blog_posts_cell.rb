d33afebb706892efed81ce6bdd018d09f84d7f7b
Vera Rojman <vrojman@protonmail.com>
2021-06-29 11:59:52 +0200

    Add latest blog posts content block

---
+++ app/cells/decidim/alternative_landing/content_blocks/latest_blog_posts_cell.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Decidim
+  module AlternativeLanding
+    module ContentBlocks
+      # A cell to be rendered as a content block with the latest blog posts published
+      # in a Decidim Organization.
+      class LatestBlogPostsCell < Decidim::ViewModel
+        include Decidim::Core::Engine.routes.url_helpers
+        include Decidim::Blogs::PostsHelper
+
+        def show
+          return if posts.empty?
+
+          render
+        end
+
+        def post_path(post)
+          Decidim::EngineRouter.main_proxy(post.component).post_path(post)
+        end
+
+        def posts
+          @posts ||= Blogs::Post.where(
+            component: blog_components.find_by(id: model.settings.blog_id) || blog_components
+          ).limit(posts_to_show).order(created_at: :desc)
+        end
+
+        private
+
+        # A MD5 hash of model attributes because is needed because
+        # it ensures the cache version value will always be the same size
+        def cache_hash
+          hash = []
+          hash << "decidim/content_blocks/latest_blog_posts"
+          hash << Digest::MD5.hexdigest(posts.map(&:cache_key_with_version).to_s)
+          hash << I18n.locale.to_s
+
+          hash.join("/")
+        end
+
+        def blog_components
+          @blog_components ||= Component.published.where(manifest_name: "blogs")
+        end
+
+        def posts_to_show
+          model.settings.count || 3
+        end
+
+        def section_title
+          translated_attribute model.settings.title
+        end
+
+        def section_link
+          link_to translated_attribute(model.settings.link_url) do
+            translated_attribute(model.settings.link_text)
+          end
+        end
+      end
+    end
+  end
+end
--- lib/decidim/alternative_landing/engine.rb
+++ lib/decidim/alternative_landing/engine.rb
@@ -105,6 +105,21 @@ module Decidim
 
           content_block.images = 1.upto(4).map { |item_number| { name: :"background_image_#{item_number}", uploader: "Decidim::AlternativeLanding::ItemImageUploader" } }
         end
+
+        Decidim.content_blocks.register(:homepage, :latest_blog_posts) do |content_block|
+          content_block.cell = "decidim/alternative_landing/content_blocks/latest_blog_posts"
+          content_block.settings_form_cell = "decidim/alternative_landing/content_blocks/latest_blog_posts_settings_form"
+          content_block.public_name_key = "decidim.alternative_landing.content_blocks.latest_blog_posts.name"
+
+          content_block.settings do |settings|
+            settings.attribute :title, type: :text, translated: true
+            settings.attribute :link_text, type: :text, translated: true
+            settings.attribute :link_url, type: :text, translated: true
+            settings.attribute :count, type: :integer, default: 3
+            settings.attribute :blog_id, type: :integer
+          end
+        end
+
       end
 
       def initialize_content_block(scope, name, attributes: [], images: [], default: false)
