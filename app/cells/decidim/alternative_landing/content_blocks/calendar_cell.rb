2fbb97c1e1d269aaf3f38134faaf0dc5aac686bc
Vera Rojman <vrojman@protonmail.com>
2021-06-30 13:51:00 +0200

    Show only events related to participatory process group

--- app/cells/decidim/alternative_landing/content_blocks/calendar_cell.rb
+++ app/cells/decidim/alternative_landing/content_blocks/calendar_cell.rb
@@ -4,32 +4,12 @@ module Decidim
   module AlternativeLanding
     module ContentBlocks
       class CalendarCell < BaseCell
-        def events
-          @events = []
-          models.map do |model|
-            model
-              .all
-              .map { |obj| @events << present(obj) if obj.organization == current_organization && present(obj).start }
-          end
-          @events
-        end
-
-        def models
-          @models ||= [
-            Decidim::Debates::Debate,
-            Decidim::Meetings::Meeting,
-            Decidim::Surveys::Survey,
-            Decidim::ParticipatoryProcessStep,
-            (Decidim::Elections::Election if defined? Decidim::Elections)
-          ].compact
+        def participatory_process_group
+          @participatory_process_group ||= Decidim::ParticipatoryProcessGroup.find(model.scoped_resource_id)
         end
 
         def resources
           @resources ||= models.map { |m| m.name.demodulize.underscore }
-        end
-
-        def present(obj)
-          Decidim::AlternativeLanding::CalendarEventPresenter.new(obj)
         end
 
         def render_events
@@ -45,6 +25,40 @@ module Decidim
               subtitle: (translated_attribute(event.subtitle) unless event.subtitle.empty?)
             }.compact
           end.to_json
+        end
+
+        private
+
+        def participatory_processes
+          Decidim::ParticipatoryProcess.where(participatory_process_group: participatory_process_group)
+        end
+
+        def components
+          Decidim::Component.where(participatory_space: participatory_processes)
+        end
+
+        def events
+          @events = []
+          models.map do |model|
+            model
+              .where(model.attribute_names.include? "decidim_component_id" ? { component: components } : { decidim_participatory_process_id: participatory_processes })
+              .map { |obj| @events << present(obj) if obj.organization == current_organization && present(obj).start }
+          end
+          @events
+        end
+
+        def models
+          @models ||= [
+            Decidim::Debates::Debate.where(component: components),
+            Decidim::Meetings::Meeting,
+            Decidim::Surveys::Survey,
+            Decidim::ParticipatoryProcessStep,
+            (Decidim::Elections::Election if defined? Decidim::Elections)
+          ].compact
+        end
+
+        def present(obj)
+          Decidim::AlternativeLanding::CalendarEventPresenter.new(obj)
         end
       end
     end
