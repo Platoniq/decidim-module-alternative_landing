e80ff4af31bb20940df7b5409246cb4366613141
Vera Rojman <vrojman@protonmail.com>
2021-07-02 13:40:29 +0200

    Update version

--- lib/decidim/alternative_landing/version.rb
+++ lib/decidim/alternative_landing/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-alternative_landing version.
   module AlternativeLanding
-    VERSION = "0.1.0"
+    VERSION = "0.2.0"
     DECIDIM_VERSION = "0.24.3"
   end
 end
