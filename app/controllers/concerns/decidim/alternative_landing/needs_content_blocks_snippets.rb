830df88350d50433ee7b67fabf9cb80aff11aef5
Ivan Vergés <ivan@platoniq.net>
2021-12-10 15:45:26 +0100

    do not show unpublished components. delegate title in surveys to the questionnaire

---
+++ app/controllers/concerns/decidim/alternative_landing/needs_content_blocks_snippets.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  module AlternativeLanding
+    module NeedsContentBlocksSnippets
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :snippets
+      end
+
+      def snippets
+        return @snippets if @snippets
+
+        @snippets = Decidim::Snippets.new
+
+        @snippets.add(:alternative_landing, ActionController::Base.helpers.stylesheet_link_tag("decidim/alternative_landing/alternative_landing.css"))
+        @snippets.add(:head, @snippets.for(:alternative_landing))
+
+        @snippets
+      end
+    end
+  end
+end
