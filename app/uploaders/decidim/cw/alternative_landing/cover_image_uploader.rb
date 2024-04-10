a6771dbfe568e9cf716274a73970d60cfe293483
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-04-10 12:34:59 +0200

    Add CW uploaders for migration

---
+++ app/uploaders/decidim/cw/alternative_landing/cover_image_uploader.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Decidim::Cw
+  module AlternativeLanding
+    # This class deals with uploading cover images to content blocks.
+    class CoverImageUploader < RecordImageUploader
+      version :big do
+        process resize_to_fit: [2880, 1620]
+      end
+
+      def max_image_height_or_width
+        8000
+      end
+    end
+  end
+end
---
+++ app/uploaders/decidim/cw/alternative_landing/item_image_uploader.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Decidim::Cw
+  module AlternativeLanding
+    # This class deals with uploading item images to content blocks.
+    class ItemImageUploader < RecordImageUploader
+      version :square do
+        process resize_to_fill: [960, 960]
+      end
+
+      version :landscape do
+        process resize_to_fill: [960, 540]
+      end
+
+      def max_image_height_or_width
+        4000
+      end
+    end
+  end
+end
