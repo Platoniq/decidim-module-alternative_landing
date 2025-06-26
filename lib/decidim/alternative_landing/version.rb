3fee6b79ee7b4b837e3e2b8d1bed41c0c78b9224
Alejandro Puerta <alejandro.puerta@nazaries.com>
2025-06-26 17:15:10 +0200

    Upgrade Decidim to 0.30.0

--- lib/decidim/alternative_landing/version.rb
+++ lib/decidim/alternative_landing/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-alternative_landing version.
   module AlternativeLanding
-    VERSION = "0.5.0"
-    DECIDIM_VERSION = "0.29.1"
-    COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
+    VERSION = "0.6.0"
+    DECIDIM_VERSION = "0.30.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.30", "< 0.31"].freeze
   end
 end
